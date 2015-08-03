a5598d850ff04ebda2e64af3397ee410631e0f98
Gregory Lavery <gregory.lavery@yahoo.com>
2015-08-03 10:58:29 -0700

    Initial Commit

---
+++ Craft Beer Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Craft Beer Map
+//
+//  Created by Gregory Lavery on 03/08/2015.
+//  Copyright (c) 2015 Gregory Lavery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
