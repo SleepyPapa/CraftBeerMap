bbf90de6af807b2d5c699f2d6cc85f9688ecc7d1
Gregory Lavery <gregory.lavery@yahoo.com>
2015-08-11 14:19:08 -0700

    scrolling enabled for main map. More to do to zoom.

--- Craft Beer Map/MapViewController.swift
+++ Craft Beer Map/MapViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController, UIScrollViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
